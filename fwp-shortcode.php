03696bc707e6c22e63aaae92d27f42e810d5cdf9
Frank Olthof <fortewebpublishing@gmail.com>
2024-04-07 13:04:21 +0200

    1.1.3 - echo replaced by html

--- fwp-shortcode.php
+++ fwp-shortcode.php
@@ -3,7 +3,7 @@
  * Plugin Name: FORTE-WP Shortcode
  * Plugin URI: https://www.forte.nl
  * Description: Defines some demo's of shortcodes
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: FORTE web publishing
  * Author URI: https://www.forte.nl
  * Text Domain: fwp
