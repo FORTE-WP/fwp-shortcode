996f9a65a73e72af2e6f0c70076a1d23257a3061
Frank Olthof <fortewebpublishing@gmail.com>
2025-01-26 02:04:20 +0100

    1.1.5 - test

--- fwp-shortcode.php
+++ fwp-shortcode.php
@@ -3,7 +3,7 @@
  * Plugin Name: FORTE-WP Shortcode
  * Plugin URI: https://www.forte.nl
  * Description: Defines some demo's of shortcodes
- * Version: 1.1.4
+ * Version: 1.1.5
  * Author: FORTE web publishing
  * Author URI: https://www.forte.nl
  * Text Domain: fwp
